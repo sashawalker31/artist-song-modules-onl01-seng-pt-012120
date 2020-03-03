9e297aa4062f6d782473437d8cce8dd988b6e990
sasha walker <sashawalker31@gmail.com>
2020-03-03 23:05:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,8 @@
-#require_relative '../lib/concerns/memorable'
+require 'pry'
 
 class Song
   attr_accessor :name
   attr_reader :artist
-  
-  extend Memorable::ClassMethods
-  include Memorable::InstanceMethods
 
   @@songs = []
 
